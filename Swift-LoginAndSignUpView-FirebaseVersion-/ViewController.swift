0cb34776b2f06873b50e8977164288d24516d1c1
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-09-27 03:49:44 +0900

    update: KeyboardNotificationでキーボード出現時にViewを持ち上げてテキストフィールドが隠れないようにする

--- Swift-LoginAndSignUpView-FirebaseVersion-/ViewController.swift
+++ Swift-LoginAndSignUpView-FirebaseVersion-/ViewController.swift
@@ -13,30 +13,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    let gradientLayer: CAGradientLayer = {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [#colorLiteral(red: 0.9294117647, green: 0.5921568627, blue: 0.3921568627, alpha: 1).cgColor, #colorLiteral(red: 0.8901960784, green: 0.1960784314, blue: 0.3098039216, alpha: 1).cgColor]
-        gradientLayer.locations = [0.0, 0.8]
-        gradientLayer.startPoint = CGPoint(x:0.5, y:0)
-        gradientLayer.endPoint = CGPoint(x:0.5, y:1.0)
-        return gradientLayer
-    }()
-    
-    // textField backgroundColor R237 G241 B251
+   
+    // textField backgroundColor R210(237) G241 B251
     override func viewDidLoad() {
         super.viewDidLoad()
-        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        view.layer.insertSublayer(gradientLayer, at: 0)
-        registerButton.layer.cornerRadius = 10
-        registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+        setUpGradientLayer()
 
         registerButton.isEnabled = false
         userNameTextField.delegate = self
         emailTextField.delegate = self
         passwordTextField.delegate = self
+        
+        // キーボード出現時の通知を受け取る
+        NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // キーボード無くなる時の通知を受け取る
+        NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func setUpGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [#colorLiteral(red: 0.9294117647, green: 0.5921568627, blue: 0.3921568627, alpha: 1).cgColor, #colorLiteral(red: 0.8901960784, green: 0.1960784314, blue: 0.3098039216, alpha: 1).cgColor]
+        gradientLayer.locations = [0.0, 0.8]
+        gradientLayer.startPoint = CGPoint(x:0.5, y:0)
+        gradientLayer.endPoint = CGPoint(x:0.5, y:1.0)
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        registerButton.layer.cornerRadius = 10
+        registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+    }
+    
+    @objc func showKeyboard(notification: Notification) {
+        let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+        // キーボード出現時にViewを持ち上げてテキストフィールドが隠れないようにする
+        guard let keyboardMinY = keyboardFrame?.minY else { return }
+        let registarButtonMaxY = registerButton.frame.maxY
+        let distance = registarButtonMaxY - keyboardMinY + 20
+        let transform = CGAffineTransform(translationX: 0, y: -distance)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [],  animations: {
+            self.view.transform = transform
+        })
+        
+    }
+    
+    @objc func hideKeyboard() {
+        // キーボードが隠れた時に元の位置に戻す
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [],  animations: {
+            self.view.transform = .identity
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
+
+
 
 extension ViewController: UITextFieldDelegate {
     func textFieldDidChangeSelection(_ textField: UITextField) {
